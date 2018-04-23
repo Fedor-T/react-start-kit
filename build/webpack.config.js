e14a0c9fc1fcab621dff0bdcb1c11be33e60247d
Fedor Tarasov <f.tarasov@sumatosoft.com>
2018-04-23 13:19:26 +0300

    update eslint rules and babel

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -56,34 +56,6 @@ config.module.rules.push({
     loader: 'babel-loader',
     query: {
       cacheDirectory: true,
-      plugins: [
-        'babel-plugin-transform-class-properties',
-        'babel-plugin-syntax-dynamic-import',
-        [
-          'babel-plugin-transform-runtime',
-          {
-            helpers: true,
-            polyfill: false, // we polyfill needed features in src/normalize.js
-            regenerator: true,
-          },
-        ],
-        [
-          'babel-plugin-transform-object-rest-spread',
-          {
-            useBuiltIns: true // we polyfill Object.assign in src/normalize.js
-          },
-        ],
-      ],
-      presets: [
-        'babel-preset-react',
-        ['babel-preset-env', {
-          modules: false,
-          targets: {
-            ie9: true,
-          },
-          uglify: true,
-        }],
-      ]
     },
   }],
 })
